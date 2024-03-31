198a7867e95dfb156f5fadd9d4bc4ed16d04def5
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-03-31 10:00:00 +0330

    getting data from getBooking for our CheckinBooking component

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -5,9 +5,11 @@ import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
 import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
+import Spinner from "../../ui/Spinner";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "../bookings/useBooking";
 
 const Box = styled.div`
   /* Box */
@@ -18,9 +20,10 @@ const Box = styled.div`
 `;
 
 function CheckinBooking() {
+  const { isLoading, booking } = useBooking();
   const moveBack = useMoveBack();
 
-  const booking = {};
+  if (isLoading) return <Spinner />;
 
   const {
     id: bookingId,
@@ -40,7 +43,7 @@ function CheckinBooking() {
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
 
-      {/* <BookingDataBox booking={booking} /> */}
+      <BookingDataBox booking={booking} />
 
       <ButtonGroup>
         <Button onClick={handleCheckin}>Check in booking #{bookingId}</Button>
