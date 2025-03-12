afe390c2d9c6c25a8f98b14b9edb6dbb11d474e9
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-12 21:36:08 +0330

    basic things of styled components

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,38 @@ const H1 = styled.h1`
   font-weight: 600;
 `;
 
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
+    <StyledApp>
       <H1>Stelix</H1>
-    </div>
+      <Button onClick={() => alert("check in")}>Check in</Button>
+      <Button onClick={() => alert("check out")}>Check out</Button>
+
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
   );
 }
 
