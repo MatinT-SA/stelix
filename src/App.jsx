bda2ee3d3f2f33e1ef9b013b89edab0356dcffd9
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-12 17:29:51 +0330

    installing and testing styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
 
 function App() {
-  return <div>Stelix</div>;
+  return (
+    <div>
+      <H1>Stelix</H1>
+    </div>
+  );
 }
 
 export default App;
