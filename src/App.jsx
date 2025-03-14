823eaaff41064261203459e58dba634bd49e0218
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-14 20:32:42 +0330

    having some other reusable components for our varition and size for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import GlobalStyle from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -15,13 +16,32 @@ function App() {
     <>
       <GlobalStyle />
       <StyledApp>
-        <Heading as="h1">Stelix</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("check in")}>Check in</Button>
-        <Button onClick={() => alert("check out")}>Check out</Button>
-        <Heading as="h3">Form</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Stelix</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button s onClick={() => alert("check in")}>
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Input type="number" placeholder="Number of guests" />
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
