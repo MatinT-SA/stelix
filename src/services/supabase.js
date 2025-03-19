b31e53db8d88606753a305aedc66fbb035279d2b
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-19 18:38:05 +0330

    uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ibgdtgbvhnopgcedszes.supabase.co";
+export const supabaseUrl = "https://ibgdtgbvhnopgcedszes.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliZ2R0Z2J2aG5vcGdjZWRzemVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNDIzNjUsImV4cCI6MjA1NzYxODM2NX0.3Nb5mmCKyC2BdSKvrsfCWqhuGcAMAlp9pyTtPjP7JqE";
 const supabase = createClient(supabaseUrl, supabaseKey);
