65f000edce6ae38d7bd55d81a1bd1c9306f2b203
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-21 21:00:20 +0330

    updating application setting

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,7 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
 import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
   const {
@@ -15,28 +16,53 @@ function UpdateSettingsForm() {
     } = {},
   } = useSettings();
 
+  const { isUpdating, updateSetting } = useUpdateSetting();
+
   if (isLoading) return <Spinner />;
+
+  function handleSetting(e, field) {
+    const { value } = e.target;
+
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
 
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          type="number"
+          disabled={isUpdating}
+          id="min-nights"
+          defaultValue={minBookingLength}
+          onBlur={(e) => handleSetting(e, "minBookingLength")}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          disabled={isUpdating}
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleSetting(e, "maxBookingLength")}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
+          disabled={isUpdating}
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleSetting(e, "maxGuestsPerBooking")}
         />
       </FormRow>
       <FormRow label="Breakfast price">
         <Input
           type="number"
+          disabled={isUpdating}
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          onBlur={(e) => handleSetting(e, "breakfastPrice")}
         />
       </FormRow>
     </Form>
