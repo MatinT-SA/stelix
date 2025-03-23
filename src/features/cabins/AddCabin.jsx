659af1e37b7c4e712dffe403d1d1742abe8eaf9e
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-23 21:05:51 +0330

    implementing the basics of modal for adding cabin

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
+
+function AddCabin() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div>
+      <Button onClick={() => setIsModalOpen((show) => !show)}>Add Cabin</Button>
+      {isModalOpen && (
+        <Modal>
+          <CreateCabinForm />
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export default AddCabin;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -4,10 +4,9 @@ import CabinTable from "../features/cabins/CabinTable";
 import { useState } from "react";
 import CreateCabinForm from "../features/cabins/CreateCabinForm";
 import Button from "../ui/Button";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
-
   return (
     <>
       <Row type="horizontal">
@@ -18,8 +17,7 @@ function Cabins() {
       <Row>
         <CabinTable />
 
-        <Button onClick={() => setShowForm((show) => !show)}>Add Cabin</Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
