9c458915dbec09ea60c3e7e8383f5d728b30910f
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-27 23:33:10 +0330

    client side filtering - filtering cabins

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -37,6 +37,7 @@ const FilterButton = styled.button`
 
 function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
 
   function handleClick(value) {
     searchParams.set(filterField, value);
@@ -49,6 +50,7 @@ function Filter({ filterField, options }) {
         <FilterButton
           key={option.value}
           onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
         >
           {option.label}
         </FilterButton>
