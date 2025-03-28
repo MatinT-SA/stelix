64fb6e92a70a6900138abf4ebd8e6dd60ffb6121
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-28 18:49:14 +0330

    getting the current sort value

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -4,13 +4,21 @@ import { useSearchParams } from "react-router-dom";
 
 function SortBy({ options }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy") || "";
 
   function handleChange(e) {
     searchParams.set("sortBy", e.target.value);
     setSearchParams(searchParams);
   }
 
-  return <Select options={options} type="white" onChange={handleChange} />;
+  return (
+    <Select
+      options={options}
+      value={sortBy}
+      type="white"
+      onChange={handleChange}
+    />
+  );
 }
 
 export default SortBy;
