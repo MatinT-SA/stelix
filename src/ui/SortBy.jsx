9873983051f3647a9cc6f586e359ea80e5e06067
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-28 18:47:32 +0330

    adding sort value to the url using searchParams hook

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Select from "./Select";
+import { useSearchParams } from "react-router-dom";
 
 function SortBy({ options }) {
-  return <Select options={options} type="white" />;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function handleChange(e) {
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  }
+
+  return <Select options={options} type="white" onChange={handleChange} />;
 }
 
 export default SortBy;
