f7204b423fecb5d3a8d63b91b9ab5b0177c8a219
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-28 18:42:50 +0330

    adding type props to the Select component using spread operator technique

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Select from "./Select";
 
 function SortBy({ options }) {
-  return <Select options={options} />;
+  return <Select options={options} type="white" />;
 }
 
 export default SortBy;
