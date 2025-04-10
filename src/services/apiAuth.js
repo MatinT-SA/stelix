86285896b4e9a640b2cde903d7251f613c3a056a
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-04-10 17:59:45 +0330

    creating our updateCurrentUser function in our apiAuth

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ email, password, fullName }) {
   const { error, data } = await supabase.auth.signUp({
@@ -42,4 +42,33 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  let updateUser;
+  if (password) updateUser = { password };
+  if (fullName) updateUser = { data: { fullName } };
+
+  const { error, data } = await supabase.auth.updateUser(updateUser);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: ErrorStorage } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (ErrorStorage) throw new Error(ErrorStorage.message);
+
+  const { error: error2, data: updatedUser } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+
+  if (error2) throw new Error(error2.message);
+
+  return updatedUser;
 }
