9b5b0f68fae4a23193c4b3dd8a6e05df7f9740de
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-04-13 22:06:21 +0330

    error boundaries

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "./Heading";
+import Button from "./Button";
+import GlobalStyle from "../styles/GlobalStyles";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +32,22 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyle />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong!</Heading>
+          <p>{error.message}</p>
+          <Button onClick={resetErrorBoundary} size="large">
+            Try Again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
