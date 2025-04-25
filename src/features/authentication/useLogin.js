df4bdac06ad97aea5f616864478c55e9153464fe
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-04-25 18:52:40 +0330

    fixing the first login doing nothing in the authentication section

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { login as loginApi } from "../../services/apiAuth";
+import { login as loginApi, getCurrentUser } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
@@ -10,8 +10,15 @@ export function useLogin() {
   const { isLoading: isLogin, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
-    onSuccess: (user) => {
-      queryClient.setQueryData(["user", user.user]);
+    onSuccess: async () => {
+      const user = await getCurrentUser();
+
+      if (!user) {
+        toast.error("Something went wrong. Try again.");
+        return;
+      }
+
+      queryClient.setQueryData(["user"], user);
       navigate("/dashboard");
     },
 
